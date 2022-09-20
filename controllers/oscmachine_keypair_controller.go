c48cd29a0c3b098004d1e54b79d421393927086a
outscale_hmi <hanen.mizouni@outscale.com>
2022-09-20 12:33:41 +0000

    update log
    Signed-off-by: outscale_hmi <hanen.mizouni@outscale.com>

--- controllers/oscmachine_keypair_controller.go
+++ controllers/oscmachine_keypair_controller.go
@@ -77,7 +77,7 @@ func reconcileKeypair(ctx context.Context, machineScope *scope.MachineScope, key
 		machineScope.Info("######### key pair Ressource id is empty  #####", "keypair", keypairSpec.Name)
 		keypairRef.ResourceMap[keypairName] = keypairName
 	}
-	machineScope.Info("@@@@@@@@@@@@@@@@@@@@@@@ Get Keypair after reconcile keypair@@@@@@@@@@@@", "keypair", keypairSpec.Name)
+	machineScope.Info("###### Get Keypair after reconcile keypair@@@@@@@@@@@@", "keypair", keypairSpec.Name)
 
 	return reconcile.Result{}, nil
 }
