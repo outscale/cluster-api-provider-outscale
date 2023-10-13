e7d75ca56bb22ace372221475334afa4cadf99d2
vincent.baer <vincent.baer@outscale.com>
2023-10-13 15:51:54 +0000

    Kubernetes change rpm and deb repo

--- hack/json-format/tests/integration_test.rs
+++ hack/json-format/tests/integration_test.rs
@@ -7,5 +7,5 @@ fn success() {
     let kubernetes_version = create_kubernetes_json();
     let mut kubernetes_image = kubernetes_version.kubernetes_output;
     let _ = kubernetes_image.drain(1..32);
-    assert_eq!(kubernetes_image, "{\"kubernetes_deb_version\":\"1.22.1-00\",\"kubernetes_rpm_version\":\"1.22.1-0\",\"kubernetes_semver\":\"v1.22.1\",\"kubernetes_series\":\"v1.22\"}");
+    assert_eq!(kubernetes_image, "{\"kubernetes_deb_version\":\"1.22.1-1.1\",\"kubernetes_rpm_version\":\"1.22.1\",\"kubernetes_semver\":\"v1.22.1\",\"kubernetes_series\":\"v1.22\"}");
 }
