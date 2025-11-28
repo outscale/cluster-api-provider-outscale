84b75f3fbd9ac8e59757c310d22a21e2478c3be3
Jean-François Bustarret <jean-francois.bustarret@outscale.com>
2025-11-28 17:08:03 +0000

    📈 api: use dev user-agent for CI calls

--- cloud/utils/version.go
+++ cloud/utils/version.go
@@ -5,11 +5,16 @@ SPDX-License-Identifier: BSD-3-Clause
 */
 package utils
 
+import "strings"
+
 var (
 	version = "dev"
 )
 
 // GetVersion retrieves the version of the provider
 func GetVersion() string {
+	if !strings.HasPrefix(version, "v") {
+		return "dev"
+	}
 	return version
 }
