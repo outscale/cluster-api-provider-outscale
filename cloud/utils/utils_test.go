029e2c6a417fab75f7dae302412e97fa694d56bf
Jean-François Bustarret <jean-francois.bustarret@outscale.com>
2026-01-20 12:29:30 +0000

    ✨ feat(OscMachine): handle tag keys having already the tags prefix

--- cloud/utils/utils_test.go
+++ cloud/utils/utils_test.go
@@ -12,12 +12,25 @@ import (
 )
 
 func TestConvertsTagsToUserDataOutscaleSection(t *testing.T) {
-	assert.Equal(t, "", ConvertsTagsToUserDataOutscaleSection(map[string]string{}))
+	t.Run("Empty tags generate an empty user data section", func(t *testing.T) {
+		assert.Equal(t, "", ConvertsTagsToUserDataOutscaleSection(map[string]string{}))
+	})
 
-	expected := `-----BEGIN OUTSCALE SECTION-----
+	t.Run("Generating a user data section with tags", func(t *testing.T) {
+		expected := `-----BEGIN OUTSCALE SECTION-----
 filter_private_section=true
 tags.key1=value1
 -----END OUTSCALE SECTION-----
 `
-	assert.Equal(t, expected, ConvertsTagsToUserDataOutscaleSection(map[string]string{"key1": "value1"}))
+		assert.Equal(t, expected, ConvertsTagsToUserDataOutscaleSection(map[string]string{"key1": "value1"}))
+	})
+
+	t.Run("tags. prefixes are trimmed", func(t *testing.T) {
+		expected := `-----BEGIN OUTSCALE SECTION-----
+filter_private_section=true
+tags.key1=value1
+-----END OUTSCALE SECTION-----
+`
+		assert.Equal(t, expected, ConvertsTagsToUserDataOutscaleSection(map[string]string{"tags.key1": "value1"}))
+	})
 }
