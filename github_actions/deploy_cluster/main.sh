8556e5eb39d238b56a1448b475b07134f6dad6a7
Jean-François Bustarret <jean-francois.bustarret@outscale.com>
2026-01-28 15:44:56 +0000

    👷 ci(action): bump installed CCM version

--- github_actions/deploy_cluster/main.sh
+++ github_actions/deploy_cluster/main.sh
@@ -103,7 +103,7 @@ done
 if [ "$CCM" = "true" ]; then
   echo "installing CCM v1"
   KUBE_MAJORMINOR=`echo $KUBERNETES_VERSION|cut -d . -f 1,2`
-  CCM_VERSION=$KUBE_MAJORMINOR.1
+  CCM_VERSION=$KUBE_MAJORMINOR.4-alpha.1
   kubectl create secret generic osc-secret --from-literal=access_key=$OSC_ACCESS_KEY --from-literal=secret_key=$OSC_SECRET_KEY -n kube-system
   helm install --wait k8s-osc-ccm oci://registry-1.docker.io/outscalehelm/osc-cloud-controller-manager --set oscSecretName=osc-secret \
     --set image.tag=$CCM_VERSION
