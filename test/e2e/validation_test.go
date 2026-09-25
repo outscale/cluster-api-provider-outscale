8ed93fef786a9e367ff402a6bd9b37a2281e978b
Jean-François Bustarret <jean-francois.bustarret@outscale.com>
2026-09-25 16:00:56 +0000

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! 🦺 validation: implement v1beta2 validation

--- test/e2e/validation_test.go
+++ test/e2e/validation_test.go
@@ -7,6 +7,9 @@ import (
 
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	kubeyaml "k8s.io/apimachinery/pkg/util/yaml"
 )
@@ -23,14 +26,29 @@ func load(file string) (*unstructured.Unstructured, error) {
 }
 
 var _ = ginkgo.Describe("[e2e][validation][fast] Testing CRD validation", func() {
-	ginkgo.BeforeEach(func() {
+	ginkgo.BeforeAll(func() {
 		gomega.Expect(bootstrapClusterProxy).ToNot(gomega.BeNil(), "Invalid argument. bootstrapClusterProxy can't be nil when validating schemas")
+
+		ns := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-cluster-api",
+			},
+		}
+		gomega.Eventually(func() error {
+			if err := bootstrapClusterProxy.GetClient().Create(ctx, ns); err != nil {
+				if apierrors.IsAlreadyExists(err) {
+					return nil
+				}
+				return err
+			}
+			return nil
+		}, "5m", "10s").Should(gomega.Succeed(), "Failed to create namespace")
 	})
 
 	ginkgo.It("should properly validate OscClusters", func() {
 		tts := []struct {
-			file  string
-			valid bool
+			file, update string
+			valid        bool
 		}{
 			{file: "testdata/osccluster/invalid/nolbname.yaml"},
 		}
@@ -38,7 +56,18 @@ var _ = ginkgo.Describe("[e2e][validation][fast] Testing CRD validation", func()
 			doc, err := load(tt.file)
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
 			err = bootstrapClusterProxy.GetClient().Create(ctx, doc)
-			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			if tt.update != "" {
+				gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+				doc, err := load(tt.update)
+				gomega.Expect(err).NotTo(gomega.HaveOccurred())
+				err = bootstrapClusterProxy.GetClient().Update(ctx, doc)
+			}
+			if tt.valid {
+				gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			} else {
+				gomega.Expect(err).To(gomega.HaveOccurred())
+			}
 		}
 	})
 })
